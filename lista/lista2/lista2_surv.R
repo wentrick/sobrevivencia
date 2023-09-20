6e8689cf41407a4bd7294a87597dc0fa48eb73f9
Wentrick <davi.w.feijo@gmail.com>
2023-09-20 17:33:39 -0300

    lista 2 e exercicios de sala

--- lista/lista2/lista2_surv.R
+++ lista/lista2/lista2_surv.R
@@ -1,4 +1,4 @@
-pacman::p_load(tidyverse,survival)
+pacman::p_load(tidyverse,survival,ggfortify)
 
 tempo = c(7,34,42,63,64,74,83,84,91,108,112,129,133,133,139,140,140,146,149,154,157,160,160,
           165,173,176,185,218,225,241,248,273,279,297,319,405,417,420,440,523,523,
@@ -6,7 +6,7 @@ tempo = c(7,34,42,63,64,74,83,84,91,108,112,129,133,133,139,140,140,146,149,154,
 censura = c(rep(1,5),0,rep(1,20),0,rep(1,5),0,1,0,rep(1,5),0,1,1,1,0,1,0,0,0,1)
 
 dados = data.frame(tempo,censura) %>%
-  arrange(tempo)
+  arrange(tempo) 
 
 
 #modelo de sobrevivencia Kaplan-meier
@@ -22,7 +22,7 @@ HHt = -log(KM$surv)
 
 plot(stepfun(KM$time[1:(length(KM$time) - 1)],c(0,HHt[1:(length(HHt) - 1)])),do.points = F)
 
-
+plot(stepfun(KM$time,c(0,HHt)),do.points = F)
 #Nelson-Aalen
 
 ENA = survfit(coxph(Surv(tempo,censura)~1, method = "breslow"))
@@ -39,6 +39,72 @@ plot(stepfun(KM$time,c(0,HHt)),do.points = F)
 #tempo mediano
 
 KM
+
+
+###############################################################################
+# exercicio 2 
+
+tempo = c(28,89,175,195,309,377,393,421,447,462,709,744,770,1106,1206,34,88,137,
+          199,280,291,299,300,309,309,351,358,369,370,375,382,392,429,451,1119)
+censura = c(rep(1,5),rep(0,4),1,rep(0,5),rep(1,6),0,0,rep(1,9),0,1,0)
+grupo = c(rep(1,15),rep(2,20))
+
+dados = data.frame(tempo,censura,grupo) %>%
+  arrange(tempo)%>%
+  mutate(grupo = as.factor(grupo))
+
+#modelo de sobrevivencia Kaplan-meier
+
+KM = survfit(Surv(dados$tempo,dados$censura)~1)
+
+plot(KM,conf.int = F, mark.time = T)
+
+summary(KM)
+
+#funcao de risco Kaplan-Meier
+HHt = -log(KM$surv)
+
+plot(stepfun(KM$time,c(0,HHt)),do.points = F)
+
+
+#Nelson-Aalen
+
+ENA = survfit(coxph(Surv(tempo,censura)~1, method = "breslow"))
+summary(ENA)
+plot(ENA, conf.int = F,)
+
+
+#funcao de risco Nelson-Aalen
+HHt = -log(ENA$surv)
+
+plot(stepfun(KM$time,c(0,HHt)),do.points = F)
+
+#analise por grupo ----- 
+
+#kaplan-meier
+
+KM = survfit(Surv(dados$tempo,dados$censura)~dados$grupo)
+
+plot(KM,conf.int = F, mark.time = T, col = c("red","blue"))
+
+#Nelson ALlen
+
+ENA = survfit(coxph(Surv(dados$tempo,dados$censura)~dados$grupo, method = "breslow"))
+summary(ENA)
+
+plot(ENA,conf.int = F, mark.time = T, col = c("red","blue"))
+
+autoplot(ENA)
+
+#tempo mediano
+
+KM
+
+
+
+
+
+
 
 
 
